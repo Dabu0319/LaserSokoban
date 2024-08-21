8abfe8ea337ea02810ca12ace5798972e4852fb7
Dabu_10 <407790750@qq.com>
2024-08-21 17:16:21 +0800

    Initial commit

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    Vector2 moveDir;
+    public LayerMask detectLayer;
+    
+    
+    public float raycastDistance = 1.5f;
+    
+    public GameObject upSprite;
+    public GameObject downSprite;
+    public GameObject leftSprite;
+    public GameObject rightSprite;
+    
+    [SerializeField]
+    private Vector2 currentDir;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+            moveDir = Vector2.right;
+
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+            moveDir = Vector2.left;
+
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+            moveDir = Vector2.up;
+
+        if (Input.GetKeyDown(KeyCode.DownArrow))
+            moveDir = Vector2.down;
+
+        if(moveDir != Vector2.zero)
+        {
+            if(CanMoveToDir(moveDir))
+            {
+                Move(moveDir);
+            }
+        }
+
+        moveDir = Vector2.zero;
+    }
+
+   
+    bool CanMoveToDir(Vector2 dir)
+    {
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, dir, raycastDistance, detectLayer);
+
+        if (!hit)
+            return true;
+        else
+        {
+            if (hit.collider.GetComponent<Box>() != null)
+                return hit.collider.GetComponent<Box>().CanMoveToDir(dir);
+        }
+
+        return false;
+    }
+
+    void Move(Vector2 dir)
+    {
+        transform.Translate(dir);
+    }
+
+    private void OnDrawGizmos()
+    {
+        
+        Gizmos.color = Color.red;
+        Gizmos.DrawLine(transform.position, (Vector2)transform.position + Vector2.down * raycastDistance);
+    }
+}
